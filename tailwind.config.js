ae1457235d364a9f9151e5dc337d9659c5fde564
Juan Carlos <jvalerio@reprintsdesk.com>
2023-09-12 09:22:27 -0600

    feat(Login): :sparkles: Finish Login view
    Create the final version for the Login view

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,24 @@ module.exports = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    theme: {
+    extend: {
       colors: {
-        "lp-primary": "#4A90E2",
-        "lp-secondary": "#A0D468",
-        "lp-secondary-action": "#F6BB42",
-        "lp-accent": "#EA6A47",
-        "lp-gray-light": "#ECECEC",
-        "lp-gray-dark": "#333333",
-        "white": "#ffffff",
+        "la-primary": "#4A90E2",
+        "la-primary-dark": "#0D509F",
+        "la-secondary": "#A0D468",
+        "la-secondary-action": "#F6BB42",
+        "la-accent": "#EA6A47",
+        "la-gray-light": "#ECECEC",
+        "la-gray-dark": "#333333",
+        white: "#ffffff",
       },
+      width: {
+        '100': '27rem',
+        '110': '29em',
+        '120': '31rem',
+        '130': '34rem',
+      }
     },
-    extend: {},
   },
   darkMode: "class",
   plugins: [nextui()],
